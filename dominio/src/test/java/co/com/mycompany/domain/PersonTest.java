7b40ecc3665cdf14a34061dc81ddd6a3cf855665
Juan Esteban <juan.castano@ceiba.com.co>
2017-10-14 01:38:43 -0500

    GRAL:Ajuste

--- dominio/src/test/java/co/com/mycompany/domain/PersonTest.java
+++ dominio/src/test/java/co/com/mycompany/domain/PersonTest.java
@@ -6,14 +6,16 @@ import org.junit.Test;
 public class PersonTest {
 
 	private static final String A_NAME = "a name";
+	private static final String A_LASTNAME = "a lastname";
 
 	@Test
 	public void createValid() {
 		//Arrange
 		Person person;
 		//Act
-		person = new Person(A_NAME);
+		person = new Person(A_NAME,A_LASTNAME);
 		//Assert
 		Assert.assertEquals(person.getName(), A_NAME);
+		Assert.assertEquals(person.getName(), A_LASTNAME);
 	}
 }
--- web/src/main/java/co/com/mycompany/service/MyService.java
+++ web/src/main/java/co/com/mycompany/service/MyService.java
@@ -1,22 +1,16 @@
 package co.com.mycompany.service;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import co.com.mycompany.domain.Person;
-
-@Path("/api/service")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@Path("/hello")
 public class MyService {
 
-	@Path("person")
-	@GET
-	public Person person(){
-		return new Person("juan esteban castaño rincon");
-	}
-
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String sayPlainTextHello() {
+    return "Hello jersey";
+  }
 }
